a164640b51f09de54ec1d5de8055bc04804061ba
Sa Eun Soo <tkdmstn0304@naver.com>
2019-06-02 18:11:12 +0900

    채팅 방에 이름 표시하기
    채팅 방에 로그인했을 때 이름 표시

--- app.js
+++ app.js
@@ -1,83 +1,45 @@
-/* 설치한 express와 socket.io모듈 불러오기 */
+
 var express = require('express');
 var socket = require('socket.io');
 var bodyParser = require('body-parser');
-
-/* Node.js 기본 내장 모듈 불러오기 */
 var http = require('http');
 var fs = require('fs');
-
-/* express 객체 생성 */
-var app = express();
-
-/*sqlite3를 불러와서 부팅과정 중 텍스트 모드로 변환*/
 var sqlite = require('sqlite3').verbose();
-/*userinfo라는 db 생성, 없으면 만들어주고 있으면 overwrite*/
-let db = new sqlite.Database('userinfo.db');
-db.run('CREATE TABLE IF NOT EXISTS userinfo(email text NOT NULL UNIQUE,username text NOT NULL UNIQUE,password text NOT NULL)');
 
-/* express http 서버 생성 */
+var app = express();
 var server = http.createServer(app);
 var io = socket(server);
 
+let db = new sqlite.Database('userinfo.db');
+db.run('CREATE TABLE IF NOT EXISTS userinfo(email text NOT NULL UNIQUE,username text NOT NULL UNIQUE,password text NOT NULL)');
 
 // app.use(cookieParser()); //사용자의 쿠키 내역 가져옴.
 app.use('/css', express.static('./public/css'))
 app.use('/js', express.static('./public/js'))
 app.use(bodyParser.urlencoded({extended: false}));
 
+app.set('views', __dirname + '/public');
+app.set('view engine', 'html');
+app.engine('html', require('ejs').renderFile);
+//app.engine('html', ejs.renderFile);
 
 /* Get 방식으로 / 경로에 접속하면 실행 됨 */
-app.get('/', function(request, response) {
-  fs.readFile('./public/login.html', function(err, data) {
-    if(err) {
-      response.send('에러')
-    } else {
-      response.writeHead(200, {'Content-Type':'text/html'})
-      response.write(data)
-      response.end()
-    }
-  })
+app.get('/', function(req, res){
+  res.render('login.html')
 })
 
-app.get('/register', function(request, response) {
-  fs.readFile('./public/register.html', function(err, data) {
-    if(err) {
-      response.send('에러')
-    } else {
-      response.writeHead(200, {'Content-Type':'text/html'})
-      response.write(data)
-      response.end()
-    }
-  })
+app.get('/register', function(req, res){
+  res.render('register.html')
 })
 
-app.get('/chat', function(request, response) {
-  fs.readFile('./public/chat.html', function(err, data) {
-    if(err) {
-      response.send('에러')
-    } else {
-      response.writeHead(200, {'Content-Type':'text/html'})
-      response.write(data)
-      response.end()
-    }
-  })
+app.get('/chat', function(req, res){
+  res.render('chat.html')
 })
 
-app.get('/login', function(request, response) {
-  fs.readFile('./public/login.html', function(err, data) {
-    if(err) {
-      response.send('에러')
-    } else {
-      response.writeHead(200, {'Content-Type':'text/html'})
-      response.write(data)
-      response.end()
-    }
-  })
+app.get('/login', function(req, res){
+  res.render('login.html')
 })
 
-
-/*post 방식으로 보내주면 페이지는 로그인 페이지로 이동*/
 app.post('/login', function(request,response){
   var name = request.body.username;
   var pw = request.body.password;
@@ -88,15 +50,17 @@ app.post('/login', function(request,response){
   }
   if(row) {			//아이디 등록이 되어있을 때
     if(row.password !== pw){ 		//비밀번호가 다를때
-	  console.log('비밀번호가 다릅니다.')
-	  console.log(row.password, pw)
-	  response.redirect('/login') }
-	else { 			//비밀번호가 같을 때
+      console.log('password err')
+      console.log(row.password, pw)
+      response.redirect('/login')
+    }
+    else { 			//비밀번호가 같을 때
       console.log(row.username, row.password, pw)
-	  response.redirect('/chat') } }
+      response.render('chat.html',{username: request.body.username}) }
+    } //이름 정보를 html에 보냄
   else {		//아이디 등록이 안되있을 때
-    console.log('login err..')
-	response.redirect('/login') }
+    console.log('id err..')
+    response.redirect('/login') }
   })
 })
 
@@ -116,18 +80,20 @@ app.post('/register', function(request, response){
   })
 })
 
+
 io.sockets.on('connection', function(socket) {
-
   /* 새로운 유저가 접속했을 경우 다른 소켓에게도 알려줌 */
-  socket.on('newUser', function(name) {
-    console.log(name + ' 님이 접속하였습니다.')
-
-    /* 소켓에 이름 저장해두기 */
-    socket.name = name
-
-    /* 모든 소켓에게 전송 */
+  socket.on('login', function(data){
+    console.log("hey")
+    console.log(data.username + "님이 접속하였습니다.")
+    socket.name  = data.username
+    name = data.username
     io.sockets.emit('update', {type: 'connect', name: 'SERVER', message: name + '님이 접속하였습니다.'})
   })
+
+  // socket.on('newUser', function(name) {
+  //   socket.name = nam
+  // })
 
   /* 전송한 메시지 받기 */
   socket.on('message', function(data) {
--- public/js/index.js
+++ public/js/index.js
@@ -1,18 +1,7 @@
 var socket = io()
 
 /* 접속 되었을 때 실행 */
-socket.on('connect', function() {
-  /* 이름을 입력받고 */
-  var name = prompt('반갑습니다!', '')
 
-  /* 이름이 빈칸인 경우 */
-  if(!name) {
-    name = '익명'
-  }
-
-  /* 서버에 새로운 유저가 왔다고 알림 */
-  socket.emit('newUser', name)
-})
 
 /* 서버로부터 데이터 받은 경우 */
 socket.on('update', function(data) {
@@ -46,7 +35,7 @@ socket.on('update', function(data) {
 function send() {
   // 입력되어있는 데이터 가져오기
   var message = document.getElementById('test' ).value
-  
+
   // 가져왔으니 데이터 빈칸으로 변경
   document.getElementById('test').value = ''
 
